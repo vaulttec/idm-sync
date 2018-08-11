68230478cf8ad5a60b3ca79bb3fde228bf7a4550
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2018-08-12 01:29:37 +0200

    switches annotation

--- src/main/java/org/vaulttec/idm/sync/idp/IdentityProviderConfig.java
+++ src/main/java/org/vaulttec/idm/sync/idp/IdentityProviderConfig.java
@@ -22,10 +22,10 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "idp")
 public class IdentityProviderConfig {
   private final Environment env;
