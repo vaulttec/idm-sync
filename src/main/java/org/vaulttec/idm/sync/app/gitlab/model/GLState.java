2290af1f2dae468298f2e6b3be1fb4214a0bfd93
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2020-05-08 11:48:42 +0200

    adds missing Gitlab user state 'LDAP_BLOCKED'

--- src/main/java/org/vaulttec/idm/sync/app/gitlab/model/GLState.java
+++ src/main/java/org/vaulttec/idm/sync/app/gitlab/model/GLState.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum GLState {
-  ACTIVE, BLOCKED;
+  ACTIVE, BLOCKED, LDAP_BLOCKED;
 
   @JsonCreator
   public static GLState fromJson(String state) {
